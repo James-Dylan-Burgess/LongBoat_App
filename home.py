aed9ec67d860e454e4f154f56cc90ff283e6f8cb
James-Dylan-Burgess <131273777+James-Dylan-Burgess@users.noreply.github.com>
2023-04-19 14:49:10 +0100

    Main files

---
+++ home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pickle
+
+        
+
+st.title(" LONGBOAT - Long Range Clinical Trial Forecasting Model ")
+
+st.write('Please select from the sidebar if you want to upload multiple studies or manually input parameters for a single trail')
